c059c066c8cec3f604e9fc1212947cdcc3ae61b7
wl_ubuntu_mynotebook <crazywl666666@gmail.com>
2021-07-25 15:57:21 +0800

    [init]init project. this is alientek uboot
    Signed-off-by: wl_ubuntu_mynotebook <crazywl666666@gmail.com>

---
+++ mx6ull_alientek_emmc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linxu-gnueabihf- mx6ull_14x14_ddr512_emmc_defconfig
+make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j12
